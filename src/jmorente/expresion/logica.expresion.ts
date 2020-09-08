fed30c8833ba2078cdc09912cc0a5e681fb4193a
José Rafael Morente González <josemorenteg98@gmail.com>
2020-09-08 00:23:17 -0600

    [FIXED] logica.expresion.ts

--- src/jmorente/expresion/logica.expresion.ts
+++ src/jmorente/expresion/logica.expresion.ts
@@ -19,20 +19,24 @@ export class Logica extends Expression {
     }
 
     public execute(environment : Environment) : Retorno{
-        console.error("LOGICA")
-        console.error(this.left)
-        console.error(this.right)
-        const leftValue = this.left.execute(environment);
-        const rightValue = this.right.execute(environment);
         if(this.type == OpcionLogica.AND){
+            const leftValue = this.left.execute(environment);
+            const rightValue = this.right.execute(environment);
             const result = leftValue.value && rightValue.value;
             return {value : result, type : Type.BOOLEAN};
         }
         else if(this.type == OpcionLogica.OR){
+            const leftValue = this.left.execute(environment);
+            const rightValue = this.right.execute(environment);
             const result = leftValue.value || rightValue.value;
             return {value : result, type : Type.BOOLEAN};
         } 
         else if(this.type == OpcionLogica.NOT){
+            console.error("EVALUCION")
+            
+            const leftValue = this.left.execute(environment);
+            console.error(leftValue.value)
+            console.error(!leftValue.value)
             const result = !leftValue.value;
             return {value : result, type : Type.BOOLEAN};
         }
